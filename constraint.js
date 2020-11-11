8e8e7ccd13a87a3c75254e5ba3960237648dcf25
Meenum2008 <73229801+Meenum2008@users.noreply.github.com>
2020-11-11 16:04:53 +0530

    Add files via upload

---
+++ constraint.js
@@ -0,0 +1,33 @@
+class ConsT{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 5
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+         
+    }
+    attach(){
+        this.sling.bodyA=this.body
+    }
+     
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+           }
+    
+            }
+        
+    }
